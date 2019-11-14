e20dc9c04800585523c7b104a050b9d998a95e6d
Sean Huntley <shuntley@shuntley-a01.vmware.com>
2019-11-14 16:21:32 +1100

    Styled the about page and fixed some errors

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './About.css';
 
 
 class About extends React.Component {
@@ -8,15 +8,19 @@ class About extends React.Component {
       <div className="page">
           <div className="heading">About this App</div>
           <div className="info">
-          This is a scalable micro services app that can be deployed in both containers and VM's on any platform. Each service can scale independently.
-          It uses data from mapbox.com, darksky.net and google maps.
-          App was built by Scott Norris 
-          <img src={require('../../public/small.jpg')} />
-          
-          and Sean Huntley. 
-          <img src={require('../../public/sean.jpg')} />
+            This is a scalable micro services app that can be deployed in both containers and VM's on any platform. Each service can scale independently.
+            It uses data from mapbox.com, darksky.net and google maps.
           </div>
-
+          <div className="contributors">
+            <div className="person">
+              This App was built by Scott Norris 
+              <img className="pimg"src={require('../images/small.jpg')} alt="Scott"/>
+            </div>
+            <div className="person">
+              and Sean Huntley 
+              <img className="pimg" src={require('../images/sean.jpg')} alt="Sean" />
+            </div>
+          </div>
       </div>
     )
   }
